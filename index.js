84a87b8cb6ceaa2d520dcea6e27d6eba38250530
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-08-19 11:21:09 +0700

    Schema

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// CONNECT TO MONGODB
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/playground')
@@ -5,3 +6,11 @@ mongoose.connect('mongodb://localhost/playground')
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
 
+// SCHEMA
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
