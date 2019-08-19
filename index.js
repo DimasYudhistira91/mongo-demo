01d75236abffde781f020f9a2678432687ece117
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-08-19 13:26:06 +0700

    saving and querying documents

--- index.js
+++ index.js
@@ -18,9 +18,39 @@ const courseSchema = new mongoose.Schema({
 
 // MODELS
 const Course = mongoose.model('Course', courseSchema);
-const course = new Course({
-  name: 'Node.js Course',
+// const course = new Course({
+//   name: 'Node.js Course',
+//   author: 'Dimas',
+//   tags: ['node', 'backend'],
+//   isPublished: true
+// });
+
+
+// Saving Document
+async function createCourse() {
+  const course = new Course({
+  name: 'Angular Course',
   author: 'Dimas',
-  tags: ['node', 'backend'],
+  tags: ['angular', 'frontend'],
   isPublished: true
 });
+
+const result = await course.save();
+console.log(result);
+}
+
+// createCourse();
+
+
+// Querying Documents
+async function getCourses() {
+  const courses = await Course
+    .find({ author: 'Dimas', isPublished: true})
+    .limit(10)
+    .sort({name: 1})
+    .select({ name: 1, tags: 1});
+
+  console.log(courses);
+}
+
+getCourses();
